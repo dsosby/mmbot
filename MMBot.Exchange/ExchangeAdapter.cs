87dcec2e1ee77666a00ea0998beb12c144b9bb02
dasosby <david@sosby.org>
2014-05-07 22:04:50 -0700

    Fix dependency for Robot reference

--- MMBot.Exchange/ExchangeAdapter.cs
+++ MMBot.Exchange/ExchangeAdapter.cs
@@ -5,9 +5,18 @@ namespace MMBot.Exchange
 {
     public class ExchangeAdapter : Adapter
     {
+        private string Email { get; set; }
+        private string Password { get; set; }
+
         public ExchangeAdapter(ILog logger, string adapterId)
             : base(logger, adapterId)
         {
+        }
+
+        private void Configure()
+        {
+            Email = Robot.GetConfigVariable("MMBOT_EXCHANGE_EMAIL");
+            Password = Robot.GetConfigVariable("MMBOT_EXCHANGE_PASSWORD");
         }
 
         public override System.Threading.Tasks.Task Run()
