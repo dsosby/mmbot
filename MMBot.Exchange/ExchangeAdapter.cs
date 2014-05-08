a51d4acc5ea8cb9ad1e562bf6d89a190ebdffd37
dasosby <david@sosby.org>
2014-05-07 21:59:53 -0700

    Adding project stub

---
+++ MMBot.Exchange/ExchangeAdapter.cs
@@ -0,0 +1,23 @@
+﻿
+using Common.Logging;
+
+namespace MMBot.Exchange
+{
+    public class ExchangeAdapter : Adapter
+    {
+        public ExchangeAdapter(ILog logger, string adapterId)
+            : base(logger, adapterId)
+        {
+        }
+
+        public override System.Threading.Tasks.Task Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override System.Threading.Tasks.Task Close()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
