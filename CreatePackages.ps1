af273285d74ed478061573bb0082fe9df436ae6c
David Sosby <dasosby@microsoft.com>
2014-05-09 18:26:10 -0700

    Add exchange nuget package

--- CreatePackages.ps1
+++ CreatePackages.ps1
@@ -7,6 +7,7 @@ if(!$version){
 msbuild .\mmbot.sln /property:Configuration=Release
 
 .\.nuget\nuget.exe pack .\mmbot.Core\mmbot.Core.csproj -Version $version -Properties Configuration=Release
+.\.nuget\nuget.exe pack .\mmbot.exchange\mmbot.exchange.csproj -Version $version -Properties Configuration=Release
 .\.nuget\nuget.exe pack .\mmbot.jabbr\mmbot.jabbr.csproj -Version $version -Properties Configuration=Release
 .\.nuget\nuget.exe pack .\mmbot.hipchat\mmbot.hipchat.csproj -Version $version -Properties Configuration=Release
 .\.nuget\nuget.exe pack .\mmbot.XMPP\mmbot.XMPP.csproj -Version $version -Properties Configuration=Release
